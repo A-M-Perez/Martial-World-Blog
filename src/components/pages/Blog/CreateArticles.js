ad645d31014c6acd0c09c552123f971c65a2984f
Martin Perez <aldomartinperez@hotmail.com>
2021-12-21 23:24:46 -0300

    Added 'Create articles' with basic functions

--- src/components/pages/Blog/CreateArticles.js
+++ src/components/pages/Blog/CreateArticles.js
@@ -2,6 +2,36 @@ import React, { useRef } from 'react';
 import '../../../styles/pages/blog/CreateArticles.css';
 import axios from 'axios';
 import { serverURL } from '../../../Global';
+import styles from '../../../styles/pages/blog/CodeOfConduct.module.css';
+
+let cocAcknowledged = false;
+
+const CodeOfConduct = () => {
+
+    function checkCodeOfConduct(e) {
+        if (e.target.checked === true) {
+            cocAcknowledged = true;
+            console.log(cocAcknowledged);
+        } else if (e.target.checked === false) {
+            cocAcknowledged = false;
+            console.log(cocAcknowledged);
+        };
+    };
+
+    return (
+        <aside className={styles.cocContainer}>
+            <h4>Code of Conduct:</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, similique placeat architecto excepturi esse atque at optio quas nesciunt quos ut nisi reiciendis quisquam, nihil laborum, expedita perspiciatis dolores quam?</p>
+            <ul>
+                <li>Rule #1</li>
+                <li>Rule #2</li>
+                <li>Rule #3</li>
+                <li>Rule #4</li>
+            </ul>
+            <input type='checkbox' name='cocAcknowledgement' onChange={checkCodeOfConduct} /><label htmlFor='cocAcknowledgement'>I acknowledge the code of conduct for posting articles.</label>
+        </aside>
+    );
+};
 
 const CreateArticles = () => {
 
@@ -12,33 +42,36 @@ const CreateArticles = () => {
 
         e.preventDefault();
 
-        const submittedArticleForm = {
-            articleTitle: articleTitle.current.value,
-            articleText: articleText.current.value
-        }
+        if (cocAcknowledged === true) {
+            const submittedArticleForm = {
+                articleTitle: articleTitle.current.value,
+                articleText: articleText.current.value
+            }
 
-        axios.post(`${serverURL}/api/post_article`, submittedArticleForm);
+            axios.post(`${serverURL}/api/post_article`, submittedArticleForm);
 
+        } else if (cocAcknowledged === false) {
+            alert('confirma el COC');
+        };
     };
 
-
     return (
-        <section>
-            <h2>CREATE ARTICLES</h2>
-            <hr />
-            <form id='createArticle' onSubmit={postArticle}>
-                <label for='articleTitle'>Title:&nbsp;</label>
-                <input type='text' name='articleTitle' placeholder="Enter article title here..." href={articleTitle} />
+        <section className='createArticleContainer'>
+            <CodeOfConduct />
+            <div className='createArticleSection'>
+                <h2>CREATE ARTICLES</h2>
+                <hr />
+                <form id='createArticle' onSubmit={postArticle}>
+                    <label htmlFor='articleTitle'>Title:&nbsp;</label>
+                    <input type='text' name='articleTitle' placeholder="Enter article title here..." href={articleTitle} />
 
-                <label>Image:&nbsp;</label>
+                    <label>Image:&nbsp;</label>
 
-                <label for='articleText'>Article text:&nbsp;</label>
-                <textarea name='articleText' placeholder="Write your article here..." href={articleText} />
-                <button type='submit'>Post</button>
-            </form>
-            <aside id="rules">
-
-            </aside>
+                    <label htmlFor='articleText'>Article text:&nbsp;</label>
+                    <textarea name='articleText' placeholder="Write your article here..." href={articleText} />
+                    <button type='submit'>Post</button>
+                </form>
+            </div>
         </section>
     )
 };
