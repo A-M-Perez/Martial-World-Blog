bbc437f305fd40244ea5ae79c53daa3d3e1020e2
Martin Perez <aldomartinperez@hotmail.com>
2022-02-14 20:09:24 -0300

    for review

--- src/Global.js
+++ src/Global.js
@@ -1,9 +1,7 @@
 const serverURL = 'https://martialworld.herokuapp.com/';
 const clientURL = 'https://martialworld.herokuapp.com/';
-const googleAPIKey = 'AIzaSyBWvJqmvWnh1wwYT53iNIGM_bZdRpc2k94';
 
 module.exports = {
     serverURL,
     clientURL,
-    googleAPIKey
 };
